89c8c9710e519f2dc101969e21ea1b11e6030ff9
leumas <leumasventures@gmail.com>
2026-05-10 19:21:23 -0400

    still d db

--- db.js
+++ db.js
@@ -30,7 +30,7 @@ const pool = mysql.createPool({
   host:               process.env.DB_HOST     || 'srv1234.hstgr.io',
   port:               parseInt(process.env.DB_PORT || '3306', 10),
   user:               process.env.DB_USER     || 'root',
-  password:           process.env.DB_PASS     || process.env.DB_PASSWORD || '',
+  password:           process.env.DB_PASS     || process.env.DB_PASSWORD || 'SAHARCO1957abadiocese',
   database:           process.env.DB_NAME     || 'u156099858_shcaba_db',
   waitForConnections: true,
   connectionLimit:    parseInt(process.env.DB_POOL_MAX || '10', 10),
